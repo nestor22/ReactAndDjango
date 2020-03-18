e4bca6bd6876b5082d0b9ba19ab567c7a6d3ca87
Nestor Gonzalez <nestor.ge222@gmail.com>
2020-03-18 16:15:43 -0700

    finanilizacion integracion de react

---
+++ leadmanager/frontend/webpack.config.js
@@ -0,0 +1,11 @@
+module.exports={
+    module:{
+        rules:{
+            test:/\.js$/,
+            exlude: /node_modules/,
+            use:{
+                loader:"babel-loader"
+            }
+        }
+    }
+}
